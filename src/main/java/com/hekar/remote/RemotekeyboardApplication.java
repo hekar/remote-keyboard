8bd018e6d05d9e20c3f352b4effa6ed90ea5ed9a
Hekar Khani <hekark@gmail.com>
2016-09-27 21:19:23 -0400

    Generated template

---
+++ src/main/java/com/hekar/remote/RemotekeyboardApplication.java
@@ -0,0 +1,12 @@
+package com.hekar.remote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemotekeyboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemotekeyboardApplication.class, args);
+	}
+}
